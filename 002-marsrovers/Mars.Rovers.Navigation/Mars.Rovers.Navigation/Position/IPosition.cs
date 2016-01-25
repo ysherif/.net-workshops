526ee8eda1933a81d2870284b2a43c449b65f5d9
Yasser Sherif <yasser.sherif@readify.net>
2016-01-25 19:21:14 +1000

    Cleaning up.

--- 002-marsrovers/Mars.Rovers.Navigation/Mars.Rovers.Navigation/Position/IPosition.cs
+++ 002-marsrovers/Mars.Rovers.Navigation/Mars.Rovers.Navigation/Position/IPosition.cs
@@ -1,11 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Mars.Rovers.Navigation
-{ 
+﻿namespace Mars.Rovers.Navigation
+{
     public interface IPosition
     {
         void incrementX(int max);
