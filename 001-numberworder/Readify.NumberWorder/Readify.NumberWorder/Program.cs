ec660fbb7156d0344359c67065d80e284e55baac
Yasser Sherif <yasser.sherif@readify.net>
2016-01-25 14:34:29 +1000

    Last comments.

--- 001-numberworder/Readify.NumberWorder/Readify.NumberWorder/Program.cs
+++ 001-numberworder/Readify.NumberWorder/Readify.NumberWorder/Program.cs
@@ -9,8 +9,6 @@ namespace Readify.NumberWorder
     {
         static void Main(string[] args)
         {
-            try
-            {
                 var numbersDictionary = new Dictionary<char, string>() {
                     { '0', "ZERO" },
                     { '1', "ONE" },
@@ -28,18 +26,14 @@ namespace Readify.NumberWorder
 
                 if (!result.Count().Equals(0))
                 {
-                    result.ForEach(o => { WriteLineWithColor(o.Message, ConsoleColor.Red); });
+                    result.ToList().ForEach(o => { WriteLineWithColor(o.Message, ConsoleColor.Red); });
                     return;
                 }
                 
                 var words = string.Join(string.Empty, args[0].Select(i => numbersDictionary[i]));
 
                 WriteLineWithColor(string.Format("Output: {0}", words), ConsoleColor.Green);
-            }
-            catch (Exception ex)
-            {
-                WriteLineWithColor(ex.Message, ConsoleColor.Red);
-            }
+            
         }
         
         private static void WriteLineWithColor(string text, ConsoleColor color)
@@ -49,20 +43,18 @@ namespace Readify.NumberWorder
             Console.ResetColor();
         }
 
-        private static List<Result> ValidateInput(string[] args)
+        private static IEnumerable<Result> ValidateInput(string[] args)
         {
             var validationResult = new List<Result>();
 
             if (args.Length == 0)
-                validationResult.Add(new Result() { Message = "Please make sure that the input is not empty." });
-
+             yield return new Result() { Message = "Please make sure that the input is not empty." };
+            
             if (args.Length > 1)
-                validationResult.Add(new Result() { Message = "Please make sure there is only one input without spaces." });
+                yield return new Result() { Message = "Please make sure there is only one input without spaces." };
 
             if (!System.Text.RegularExpressions.Regex.IsMatch(args[0], "^[0-9]*$"))
-                validationResult.Add(new Result() { Message = "Please make sure that the input contains only numbers." });
-
-            return validationResult;
+                yield return new Result() { Message = "Please make sure that the input contains only numbers." };
         }
 
         }
