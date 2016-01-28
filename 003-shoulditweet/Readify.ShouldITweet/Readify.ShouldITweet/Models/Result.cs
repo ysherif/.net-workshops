1a49803712cb1aedabe95033348740834e4cb072
Yasser Sherif <yasser.sherif@readify.net>
2016-01-28 12:29:51 +1000

    Should I tweet?

---
+++ 003-shoulditweet/Readify.ShouldITweet/Readify.ShouldITweet/Models/Result.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Readify.ShouldITweet.Models
+{
+    public class Result
+    {
+        public string Message { get; set; }
+       
+        public Result(string message)
+        {
+            Message = message;
+        }
+
+        public Result()
+        {
+            Message = string.Empty;
+
+        }
+    }
+}
