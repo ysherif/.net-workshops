d67aa4fd5814e5e873c3aca0eae8854f20b82be0
Yasser Sherif <yasser.sherif@readify.net>
2016-01-28 12:50:18 +1000

    Handling empty tweets.

--- 003-shoulditweet/Readify.ShouldITweet/Readify.ShouldITweet/Controllers/HomeController.cs
+++ 003-shoulditweet/Readify.ShouldITweet/Readify.ShouldITweet/Controllers/HomeController.cs
@@ -27,17 +27,20 @@ namespace Readify.ShouldITweet.Controllers
             List<string> notAllowedWords = new List<string>() {"Trump","Selfie"};
 
             if (tweet.Length > 140)
-                return new Result("Tweet must be less than or equal 140 characters.");
+                return new Result("The tweet must be less than or equal 140 characters.");
+
+            if (string.IsNullOrEmpty(tweet))
+                return new Result("Please type in a tweet and try again.");
 
             foreach (var word in notAllowedWords)
             {
                 if(tweet.ToLower().Split(' ').FirstOrDefault(o=>o == word.ToLower()) != null)
                 {
-                    return new Result($"Tweet contains word '{word}' which is not appropriate.");
+                    return new Result($"The tweet contains word '{word}' which is not appropriate.");
                 }
             }
 
-            return new Result("Tweet doesn't contain any inappropriate words. You can post it!");   
+            return new Result("The tweet doesn't contain any inappropriate words. You can post it!");   
         }
     }
 }
