183923cd6f7cd0e3ed86075918b04402666d6113
Yasser Sherif <yasser.sherif@readify.net>
2016-01-31 13:52:39 +1000

    initial commit.

---
+++ 005-shoulditweet-client/2/Readify.ShouldITweetClient/Readify.ShouldITweetClient/DAL/WordInitializer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Readify.ShouldITweet2.DAL
+{
+    using Models;
+    public class WordInitializer : System.Data.Entity.DropCreateDatabaseIfModelChanges<WordContext>
+    {
+        protected override void Seed(WordContext context)
+        {
+            var words = new List<Word>
+            {
+            new Word{WordText = "Trump" },
+            new Word{WordText = "Selfie"},
+            };
+
+            words.ForEach(w => context.Words.Add(w));
+            context.SaveChanges();
+        }
+    }
+    
+}
---
+++ 005-shoulditweet-client/2/Readify.ShouldITweetClient/Readify.ShouldITweetClient/Scripts/app/tweet-controller.js
@@ -0,0 +1,8 @@
+﻿angular.module('TweetApp', [])
+    .controller('TweetCtrl', function ($scope, $http) {
+        $scope.answered = false;
+
+        $scope.answer = function () {
+            alert('Hi');
+        };
+    });
