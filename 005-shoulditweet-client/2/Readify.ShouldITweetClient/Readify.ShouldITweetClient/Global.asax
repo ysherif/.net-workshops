492e014df254209237216188863b74dc882c6264
Yasser Sherif <yasser.sherif@readify.net>
2016-01-31 13:52:39 +1000

    initial commit.

---
+++ 005-shoulditweet-client/2/Readify.ShouldITweetClient/Readify.ShouldITweetClient/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Readify.ShouldITweetClient.MvcApplication" Language="C#" %>
---
+++ 005-shoulditweet-client/2/Readify.ShouldITweetClient/Readify.ShouldITweetClient/Models/Word.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Readify.ShouldITweet2.Models
+{
+    public class Word
+    {
+        public int WordID { get; set; }
+
+        [Required]
+        [DisplayName("Word")]
+        public string WordText { get; set; }
+        
+    }
+}
